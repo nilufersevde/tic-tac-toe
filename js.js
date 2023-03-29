5206109f3b362e4ce1ef28213f1a7df71a151f0a
Nilufer <n.sevde@hotmail.com>
2023-03-29 12:23:44 +0300

    fix the issue with round, add style to the game

--- js.js
+++ js.js
@@ -1,7 +1,6 @@
 //DOM elements
 const squares = document.querySelectorAll(".square");
 const button = document.querySelector("#button");
-const winner = document.querySelector("#winner");
 const big_container = document.querySelector(".big_container");
 const turn = document.querySelector(".turn")
 
@@ -14,6 +13,10 @@ const Player = (name, sign) => {
 const game = (() => {
     let board = ["", "", "", "", "", "", "", "", ""];
     let round = 0;
+    let winnerDeclared = false;
+    const playerOne = Player("player X", "X");
+    const playerTwo = Player("player O", "O");
+    
 
     const render = () => { for (let i=0; i<board.length ; i++){
         squares[i].innerHTML = board[i];
@@ -23,21 +26,17 @@ const game = (() => {
         round=0;
         board = ["", "", "", "", "", "", "", "", ""];
         render();
-        button.style.visibility = "hidden";
-        winner.style.visibility = "hidden";
-        big_container.classList.remove("blur");
+        turn.innerHTML = "";
+        turn.innerHTML = "It is playerX's turn";
+        winnerDeclared = false;
+        squares.forEach(item => item.style.backgroundColor="rgb(126, 231, 245)")
+        button.style.backgroundColor = "rgb(126, 231, 245)";
+        turn.style.color = "aliceblue";
+        turn.style.fontSize = "25px";
+        turn.style.textShadow = "none"
     }
-    
+
     button.addEventListener("click",restartGame);
-
-    const resetGame = () => {
-        button.style.visibility = "visible";
-        winner.style.visibility = "visible";
-        big_container.classList.add("blur");
-    }
-
-    const playerOne = Player("player X", "X");
-    const playerTwo = Player("player O", "O");
 
     const winnerCheck = (currentPlayer) => {
 
@@ -55,34 +54,52 @@ const game = (() => {
         for (condition of winningConditions) {
             if (board[condition[0]] == board[condition[1]]&& board[condition[1]] == board[condition[2]]&&
                 board[condition[0]] != ''){
-                resetGame();
-                winner.innerHTML = `The winner is ${currentPlayer.name}`;
-                console.log(currentPlayer);
-            }
+                squares[condition[0]].style.backgroundColor="rgb(36, 237, 18)";
+                squares[condition[1]].style.backgroundColor="rgb(36, 237, 18)";
+                squares[condition[2]].style.backgroundColor="rgb(36, 237, 18)";
+                button.style.backgroundColor="rgb(36, 237, 18)";
+                turn.innerHTML = "";
+                turn.innerHTML = `The winner is ${currentPlayer.name}!`;
+                turn.style.fontSize = "30px";
+                turn.style.color = "red";
+                turn.style.textShadow = "1px 1px white"
+                winnerDeclared = true;
+            }}}
 
-            else if (round>=9) {
-                resetGame()
-                winner.innerHTML = `It is a tie!`;
+     
+    const roundCheck = (round) => {
+            if (round>=9) {
+                winnerDeclared = true;
+                turn.innerHTML = "";
+                turn.innerHTML = `It is a tie!`;
+                button.style.backgroundColor="rgb(36, 237, 18)";
+                turn.style.fontSize = "30px";
+                turn.style.color = "red";
+                turn.style.textShadow = "1px 1px white"
             }
         }
-    }
+
     
     squares.forEach(item => 
         item.addEventListener('click',() => {
-            if (item.innerHTML) return;
+            if (item.innerHTML !="" || winnerDeclared == true) return;
+                console.log(winnerDeclared)
                 round++
                 const currentPlayer = ( round % 2 === 1 ? playerOne : playerTwo);
+                if (currentPlayer == playerOne) {item.style.color="rgb(142, 86, 195)"}
+                else {item.style.color = "rgb(177, 72, 72)" };
                 const nextPlayer = ( round % 2 === 1 ? playerTwo : playerOne);
                 turn.innerHTML = "";
                 turn.innerHTML = `It is ${nextPlayer.name}'s turn`;
                 currentSign = currentPlayer.sign;
                 const index = item.dataset.index;
                 board[index]=currentSign;
-                render()
+                render();
                 winnerCheck(currentPlayer);
+                roundCheck(round);
+
     }))
     return {round, board, restartGame}
-
 })()
 
 
